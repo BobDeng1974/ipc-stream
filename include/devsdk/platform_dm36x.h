21ba147d834bd111601eed73bd0d8cdaf12542bc
liyq <liyuanquan@qiniu.com>
2018-12-26 11:06:46 +0800

    delete devsdk

--- include/devsdk/platform_dm36x.h
+++
@@ -1,9 +0,0 @@
-#ifndef _____PLATFORM_H____
-#define _____PLATFORM_H____
-
-#define PLATFORM_HI35XX						0
-#define PLATFORM_DM365						1
-
-#endif
-
-
--- include/devsdk/platform_hi35xx.h
+++
@@ -1,9 +0,0 @@
-#ifndef _____PLATFORM_H____
-#define _____PLATFORM_H____
-
-#define PLATFORM_HI35XX						1
-#define PLATFORM_DM365						0
-
-#endif
-
-
